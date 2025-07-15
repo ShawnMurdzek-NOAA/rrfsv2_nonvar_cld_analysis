534fc634e80b8f9bdfe2ecba12ee4402572ee0b0
ShawnMurdzek-NOAA <shawn.s.murdzek@noaa.gov>
2025-07-15 23:12:55 +0000

    Prep to adapt metarcld.fd to MPAS
    - Copy additional f90 files so metarcld.fd can run on its own
    - Add first-draft script for compiling code
    - Add README
    - Save a copy of the files that we plan on changing

---
+++ metarcld.fd/build_metarcld.sh
@@ -0,0 +1,40 @@
+
+env_dir='../env/'
+map_util_dir='../wps_map_utils/'
+machine='ursa'
+
+echo 'Cleaning...'
+rm *.o
+rm *.mod
+
+echo 'Configuring Environment...'
+source ${env_dir}/${machine}.env
+module list
+echo
+
+echo 'Building Program...'
+compiler=mpiifort
+
+# diag-disable flag disables a warning about how ifort is deprecated
+flags='-diag-disable=10448 -traceback'
+${compiler} -c ${flags} kinds.f90
+${compiler} -c ${flags} constants.f90
+${compiler} -c ${flags} mpasio.f90 -I ${netcdf_fortran_ROOT}/include
+${compiler} -c ${flags} cld_parm_array_module.f90
+${compiler} -c ${flags} reorg_metar_cloud_regular.f90
+${compiler} -c ${flags} read_prepbufr_metarcld.f90
+if [[ ${machine} == 'ursa' ]]; then
+  ${compiler} ${flags} process_metar_cloud.f90 -o process_metarcld.exe \
+  	  ./kinds.o ./constants.o ./mpasio.o ./cld_parm_array_module.o \
+  	  ${map_util_dir}/misc_definitions_module.o ${map_util_dir}/module_map_utils.o \
+ 	  -I${map_util_dir} \
+	  -L${netcdf_fortran_ROOT}/lib -lnetcdff \
+	  -L${bufr_ROOT}/lib64 -lbufr_4
+else
+  ${compiler} ${flags} process_metar_cloud.f90 -o process_metarcld.exe \
+  	  ./kinds.o ./constants.o ./mpasio.o ./cld_parm_array_module.o \
+  	  ${map_util_dir}/misc_definitions_module.o ${map_util_dir}/module_map_utils.o \
+ 	  -I${map_util_dir} \
+	  -L${netcdf_fortran_ROOT}/lib -lnetcdff \
+	  -L${bufr_ROOT}/lib64 -lbufr_d
+fi
